d716a5f850604d5036efd18624d406409e9b74c2
Naveen Miriyalu <nmiriyal@redhat.com>
2025-06-29 15:02:05 +0000

    Offline harness for Llama2-70B

--- language/llama2-70b/dataset.py
+++ language/llama2-70b/dataset.py
@@ -73,12 +73,14 @@ class Dataset:
         self.attention_masks = []
 
         for ids in input_tokens:
-            input_ids = torch.tensor(ids, dtype=torch.int32).view(
-                1, -1).to(self.device)
-            attn_mask = torch.ones_like(input_ids)
-            self.input_ids.append(input_ids)
-            self.attention_masks.append(attn_mask)
-            self.input_lens.append(input_ids.shape[-1])
+            #input_ids = torch.tensor(ids, dtype=torch.int32).view(
+            #    1, -1).to(self.device)
+            #attn_mask = torch.ones_like(input_ids)
+            #self.input_ids.append(input_ids)
+            #self.attention_masks.append(attn_mask)
+            #self.input_lens.append(input_ids.shape[-1])
+            self.input_ids.append(ids)
+            self.input_lens.append(len(ids))
         print("Finished loading dataset.")
 
     def postProcess(
